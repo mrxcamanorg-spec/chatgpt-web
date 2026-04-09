4ac21955fe0e607e251d0396b59219830187bca1
mrxcamanorg-spec <mrxcamanorg@gmail.com>
2026-04-09 16:26:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// DARK MODE
 function toggleDark() {
-  document.body.classList.toggle("dark");
+  document.body.classList.toggle("dark-mode");
 }
+
+// 📱 IMAGE SLIDER
+let images = ["phone1.jpg","phone2.jpg","phone3.jpg"];
+let i = 0;
+
+setInterval(() => {
+  i++;
+  if(i >= images.length) i = 0;
+  document.getElementById("slide").src = images[i];
+}, 2000);
